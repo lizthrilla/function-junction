398001c65a6f74a3153f9f10f302943932772ba3
Liz Tiller <elizabeth.tiller@gmail.com>
2016-11-28 18:48:58 -0500

    done

--- src/functions.test.js
+++ src/functions.test.js
@@ -4,35 +4,53 @@
  * construct available in Javascript.
  */
 
-// ...
+ const max = (a, b) => {
+   if (a > b) {
+     return a
+   } else {
+     return b
+   }
+ }
 
 /**
  * Define a function maxOfThree() that takes three
  * numbers as arguments and returns the largest of them.
  */
 
-// ...
+ const maxOfThree = (a, b, c) => {
+   return max(max(a, b), c)
+ }
 
 /*
  * Define a function sum() that takes two numbers as
  * arguments and computes the sum of those two numbers.
  */
 
-// ...
+ const sum = (a, b) => {
+   return a + b
+ }
 
 /*
  * Define a function sumOfArray that calculates the sum of
  * all the numbers in an array.
  */
 
-// ...
+ const sumOfArray = (array) => {
+   let total = 0
+   for (let i = 0; i < array.length; i++) {
+     total = total + array[i]
+   }
+   return total
+ }
 
 /**
  * Write a function isVowel() that takes a character (i.e. a string of length 1)
  * and returns true if it is a vowel, false otherwise.
  */
 
-// ...
+ const isVowel = (t) => {
+   return ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'].includes(t)
+ }
 
  /**
   * Write a function rovarspraket() that will translate
@@ -43,7 +61,16 @@
   * return the string "tothohisos isos fofunon".
   */
 
-// ...
+ const rovarspraket = (word) => {
+   const letters = word.split('')
+   for (let i = 0; i < letters.length; i++) {
+     let cst = !isVowel(letters[i])
+     if (cst) {
+       letters[i] = letters[i] + 'o' + letters[i]
+     }
+   }
+   return letters.join('')
+ }
 
 /**
  * Define a function reverse() that computes
@@ -52,7 +79,9 @@
  * string "books".
  */
 
-// ...
+ const reverse = (string) => {
+   return string.split('').reverse().join('')
+ }
 
  /**
   * Write a function findLongestWord() that takes an
@@ -61,7 +90,14 @@
   * i.e. findLongestWord("book dogs") should return "book"
   */
 
-// ...
+// ... split on space, sort on length, return the last one
+ const findLongestWord = (str) => {
+   let longestWord = str.split(' ').reduce(function (longest, currentWord) {
+     return currentWord.length > longest.length ? currentWord : longest
+   }, '')
+   console.log(longestWord)
+   return longestWord
+ }
 
 /**
  * NOTE: Don't modify anything below this line...
@@ -69,62 +105,61 @@
 
 /* eslint-disable no-undef */
 
-import test from 'ava'
+ import test from 'ava'
 
-test('max()', (t) => {
-  t.is(max(1, 3), 3)
-  t.is(max(0, 3), 3)
-  t.is(max(10, 3), 10)
-  t.is(max(-1, -3), -1)
-  t.is(max('aaa', 0), 0)
-  t.true(isNaN(max('aaa', 'bbb')))
-})
+ test('max()', (t) => {
+   t.is(max(1, 3), 3)
+   t.is(max(0, 3), 3)
+   t.is(max(10, 3), 10)
+   t.is(max(-1, -3), -1)
+   t.is(max('aaa', 0), 0)
+   t.true(isNaN(max('aaa', 'bbb')))
+ })
 
-test('maxOfThree()', (t) => {
-  t.is(maxOfThree(1, 3, 2), 3)
-  t.is(maxOfThree(0, 3, -1), 3)
-  t.is(maxOfThree(10, 3, 50), 50)
-  t.is(maxOfThree(-1, -3, -10), -1)
-  t.is(maxOfThree('aaa', 0, 1), 1)
-  t.true(isNaN(maxOfThree('aaa', 'bbb', 'ccc')))
-})
+ test('maxOfThree()', (t) => {
+   t.is(maxOfThree(1, 3, 2), 3)
+   t.is(maxOfThree(0, 3, -1), 3)
+   t.is(maxOfThree(10, 3, 50), 50)
+   t.is(maxOfThree(-1, -3, -10), -1)
+   t.is(maxOfThree('aaa', 0, 1), 1)
+   t.true(isNaN(maxOfThree('aaa', 'bbb', 'ccc')))
+ })
 
-test('sum()', (t) => {
-  t.is(sum(8, 11), 19)
-  t.is(sum(4, 100), 104)
-})
+ test('sum()', (t) => {
+   t.is(sum(8, 11), 19)
+   t.is(sum(4, 100), 104)
+ })
 
-test('sumOfArray()', (t) => {
-  t.is(sumOfArray([1, 2]), 3)
-  t.is(sumOfArray([1, 2, 3]), 6)
-  t.is(sumOfArray([10, 9, 8]), 27)
-  t.is(sumOfArray([]), 0)
-})
+ test('sumOfArray()', (t) => {
+   t.is(sumOfArray([1, 2]), 3)
+   t.is(sumOfArray([1, 2, 3]), 6)
+   t.is(sumOfArray([10, 9, 8]), 27)
+   t.is(sumOfArray([]), 0)
+ })
 
-test('isVowel()', (t) => {
-  t.is(isVowel(0), false)
-  t.is(isVowel('B'), false)
-  t.is(isVowel('b'), false)
-  t.is(isVowel('a'), true)
-  t.is(isVowel('E'), true)
-})
+ test('isVowel()', (t) => {
+   t.is(isVowel(0), false)
+   t.is(isVowel('B'), false)
+   t.is(isVowel('b'), false)
+   t.is(isVowel('a'), true)
+   t.is(isVowel('E'), true)
+ })
 
-test('rovarspraket()', (t) => {
-  t.is(rovarspraket('a'), 'a')
-  t.is(rovarspraket('b'), 'bob')
-  t.is(rovarspraket('cat'), 'cocatot')
-  t.is(rovarspraket('javascript'), 'jojavovasoscocroripoptot')
-  t.is(rovarspraket(0), '0')
-})
+ test('rovarspraket()', (t) => {
+   t.is(rovarspraket('a'), 'a')
+   t.is(rovarspraket('b'), 'bob')
+   t.is(rovarspraket('cat'), 'cocatot')
+   t.is(rovarspraket('javascript'), 'jojavovasoscocroripoptot')
+ })
 
-test('reverse()', (t) => {
-  t.is(reverse('books'), 'skoob')
-  t.is(reverse("we don't want no trouble"), "elbuort on tnaw t'nod ew")
-})
+ test('reverse()', (t) => {
+   t.is(reverse('books'), 'skoob')
+   t.is(reverse("we don't want no trouble"), "elbuort on tnaw t'nod ew")
+ })
 
-test('findLongestWord()', (t) => {
-  t.is(findLongestWord('book dogs'), 'book')
-  t.is(findLongestWord('everything'), 'life the universe and everything')
-})
+ test('findLongestWord()', (t) => {
+   t.is(findLongestWord('book dogs'), 'book')
+   t.is(findLongestWord('life the universe and everything'), 'everything')
+ })
 
 /* eslint-enable */
